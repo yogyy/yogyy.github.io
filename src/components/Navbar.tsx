9e2d1d6dd2e4dd827afc4a3f767ea11b7a64639e
yogyy <yogimr261@gmail.com>
2024-01-29 00:04:04 +0700

    chore: update component

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,19 +1,23 @@
 const navlink = [
-  { name: 'home', link: 'yogyy.vercel.app' },
-  { name: 'blog', link: 'yogyy.vercel.app/posts' },
-  { name: 'guestbook', link: 'burungbiru.vercel.app' }
-]
+  { name: "home", link: "yogyy.vercel.app" },
+  { name: "blog", link: "yogyy.vercel.app/posts" },
+  { name: "guestbook", link: "burungbiru.vercel.app" },
+];
 
 export default function Navbar() {
-
   return (
-    <nav className="text-sm font-medium mt-8 mb-16 w-fit">
-      <ul className="flex flex-row gap-3" >
+    <nav className="mb-16 mt-8 w-fit text-sm font-medium">
+      <ul className="flex flex-row gap-3">
         {navlink.map((nav, i) => (
           <li key={nav.name}>
-            <a className='relative' href={`https://${nav.link}`} target='_blank'>
+            <a
+              className="relative"
+              href={`https://${nav.link}`}
+              target="_blank">
               {nav.name}
-              {i === 0 && <div className="absolute h-[1px] top-5 inset-0 bg-gradient-to-r from-transparent to-white w-full" />}
+              {i === 0 && (
+                <div className="absolute inset-0 top-5 h-[1px] w-full bg-gradient-to-r from-transparent to-white" />
+              )}
             </a>
           </li>
         ))}
