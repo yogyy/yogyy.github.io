b7e8311435b10786e89374fceef213dedf94a50a
yogyy <yogimr261@gmail.com>
2024-01-29 00:01:40 +0700

    chore: update Gallery

--- src/components/items.ts
+++ src/components/items.ts
@@ -1,34 +1,34 @@
 export const navbar = [
-  'Home',
-  'Our Service',
-  'Portofolio',
-  'Testimonial',
-  'Contact Us',
+  "Home",
+  "Our Service",
+  "Portofolio",
+  "Testimonial",
+  "Contact Us",
 ];
 
 export const images = [
   {
-    name: 'Kobeni Chainsaw Man pict',
-    link: 'https://pbs.twimg.com/media/FzqpKQzaMAAbrWs?format=webp&name=small',
+    name: "Kobeni",
+    link: "https://pbs.twimg.com/media/FzqpKQzaMAAbrWs?format=webp&name=small",
   },
   {
-    name: 'Reze Chainsaw Man pict',
-    link: 'https://pbs.twimg.com/media/F2VJahDaYAAgh1L?format=webp&name=small',
+    name: "Reze",
+    link: "https://pbs.twimg.com/media/F2VJahDaYAAgh1L?format=webp&name=small",
   },
   {
-    name: 'Quanxi Chainsaw Man pict',
-    link: 'https://pbs.twimg.com/media/FytE3IdaMAArwWP?format=webp&name=small',
+    name: "Quanxi",
+    link: "https://pbs.twimg.com/media/FytE3IdaMAArwWP?format=webp&name=small",
   },
   {
-    name: 'Makima Chainsaw Man pict',
-    link: 'https://pbs.twimg.com/media/Fzqpa77aYAEZYng?format=webp&name=small',
+    name: "Makima",
+    link: "https://pbs.twimg.com/media/Fzqpa77aYAEZYng?format=webp&name=small",
   },
   {
-    name: 'Yoru Chainsaw Man pict',
-    link: 'https://pbs.twimg.com/media/FzqwvloaIAAHoYM?format=webp&name=small',
+    name: "Famine",
+    link: "https://pbs.twimg.com/media/F45xxUYbQAA0_ex?format=webp&name=small",
   },
   {
-    name: 'Famine Chainsaw Man pict',
-    link: 'https://pbs.twimg.com/media/F45xxUYbQAA0_ex?format=webp&name=small',
+    name: "Yoru",
+    link: "https://pbs.twimg.com/media/F0UGZHqaUAEdR-V?format=webp&name=small",
   },
 ];
