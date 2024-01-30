46038cd7be07fda0a0929bb57b1dcd1a21348dde
yogyy <yogimr261@gmail.com>
2024-01-30 11:13:16 +0700

    chore: update repo

--- src/components/Repository.tsx
+++ src/components/Repository.tsx
@@ -1,4 +1,4 @@
-import Repo from "./repo-react";
+import { Repo } from "./repo-react";
 import type { Endpoints } from "@octokit/types";
 import useSWR from "swr";
 import { fetcher } from "@/lib/utils";
@@ -7,21 +7,21 @@ import { Skeleton } from "./ui/skeleton";
 type Repos = Endpoints["GET /users/{username}/repos"]["response"]["data"];
 export function Repos() {
   const { data, isLoading } = useSWR<Repos>(
-    "https://api.github.com/users/yogyy/repos?per_page=6&sort=updated",
+    "https://api.github.com/users/yogyy/repos?per_page=6&sort=pushed",
     fetcher,
   );
 
   return (
     <div className="my-8">
-      <h1 className="text-base">latest projects i have worked on</h1>
+      <h1 className="text-base">latest code i've push to github.</h1>
       <ul className="gallery relative mt-2 columns-1 gap-0 space-y-3 sm:columns-2">
         {isLoading
           ? [...Array(6)].map((_, i) => (
-              <div key={i} className="px-0 sm:px-2">
-                <Skeleton className="box h-[80px] w-full" />
-              </div>
+              <li key={i} className="px-0 sm:px-2">
+                <Skeleton className="box h-[96.39px] w-full" />
+              </li>
             ))
-          : data?.map((repo, i) => (
+          : data?.map((repo) => (
               <Repo key={repo.id} repo={repo} className="px-0 sm:px-2" />
             ))}
       </ul>
--- src/components/repo-react.tsx
+++ src/components/repo-react.tsx
@@ -1,11 +1,17 @@
 import { cn } from "@/lib/utils";
 import type { Endpoints } from "@octokit/types";
-import colors from "@/lib/colors.json";
+import colors from "@/constants/colors.json";
 import { Fork, Grab, Repo as RepoIcon, Star } from "./icons";
+import {
+  Tooltip,
+  TooltipContent,
+  TooltipProvider,
+  TooltipTrigger,
+} from "@/components/ui/tooltip";
 interface RepoProps extends React.LiHTMLAttributes<HTMLLIElement> {
   repo: Endpoints["GET /users/{username}/repos"]["response"]["data"][number];
 }
-export default function Repo(props: RepoProps) {
+export function Repo(props: RepoProps) {
   const { className, repo } = props;
   const color = Object.values(colors).find(
     (lang) => lang.url === `https://github.com/trending?l=${repo.language}`,
@@ -21,19 +27,27 @@ export default function Repo(props: RepoProps) {
               <RepoIcon />
               <a
                 href={repo.html_url}
-                className="mr-1 break-words text-[14px] font-semibold text-[#58a6ff] hover:underline">
+                className="mr-1 break-words font-[GeistMedium] text-[14px] text-[#58a6ff] hover:underline">
                 {repo.name}
               </a>
-              <span className="inline rounded-[2em] border border-[#30363d] p-[.12em_.5em] text-[11px] leading-4">
+              <span className="inline rounded-[2em] border border-[#30363d] p-[.11em_.5em] text-[11px] leading-4">
                 {repo.private ? "Private" : "Public"}
               </span>
             </div>
-            <div className="hover:cursor-grab">
-              <Grab />
-            </div>
+            <TooltipProvider>
+              <Tooltip>
+                <TooltipTrigger className="hover:cursor-grab">
+                  <Grab />
+                </TooltipTrigger>
+                <TooltipContent className="text-balance border border-[#30363d] text-[#8b949e]">
+                  drag and drop library not worked <br /> on astro with react
+                  integration
+                </TooltipContent>
+              </Tooltip>
+            </TooltipProvider>
           </div>
           <p className="mt-2 inline-flex text-xs">{repo.description}</p>
-          <p className="mt-2 inline-flex items-center text-xs">
+          <p className="mt-2 inline-flex items-center text-xs leading-4">
             <span className="mr-3 inline-flex gap-1">
               <span
                 className="repo-language-color"
@@ -53,7 +67,7 @@ export default function Repo(props: RepoProps) {
               <a
                 href={`${repo.html_url}/forks`}
                 target="_blank"
-                className="ml-4 flex items-center justify-end gap-1 [&>svg]:fill-[#8b949e]">
+                className="ml-4 flex items-center justify-center gap-1 [&>svg]:fill-[#8b949e]">
                 <Fork />
                 {repo.forks_count}
               </a>
