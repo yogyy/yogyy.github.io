d69c7b8af7ac56508a959702f08673d31faf2beb
yogyy <yogimr261@gmail.com>
2024-02-24 00:09:00 +0700

    faet: add sitemap
    add googe site verification

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,6 +1,7 @@
 import { defineConfig } from "astro/config";
 import tailwind from "@astrojs/tailwind";
 import react from "@astrojs/react";
+import sitemap from "@astrojs/sitemap";
 
 // https://astro.build/config
 export default defineConfig({
@@ -9,6 +10,7 @@ export default defineConfig({
     tailwind({
       applyBaseStyles: false,
     }),
+    sitemap(),
     react(),
   ],
   image: {
