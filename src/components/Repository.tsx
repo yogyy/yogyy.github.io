c7a15edfcebb0e17e7841e175e832d7dcfb7c41e
yogyy <yogimr261@gmail.com>
2025-06-25 19:40:57 +0700

    chore: update repo component

--- src/components/Repository.tsx
+++ src/components/Repository.tsx
@@ -15,15 +15,17 @@ export function Repos() {
   return (
     <div className="my-4">
       <h1 className="my-2 text-base">latest code i've push to github.</h1>
-      <ul className="grid w-full grid-cols-1 gap-y-4 sm:grid-cols-2">
+      <ol className="flex flex-wrap">
         {isLoading
           ? [...Array(6)].map((_, i) => (
-              <li key={i} className="px-0 sm:px-2">
+              <li
+                key={i}
+                className="mb-4 w-full px-0 sm:w-1/2 sm:min-w-[272px] sm:px-2">
                 <Skeleton className="box h-[96.39px] w-full" />
               </li>
             ))
           : data?.map((repo) => <Repo key={repo.id} repo={repo} />)}
-      </ul>
+      </ol>
     </div>
   );
 }
@@ -34,7 +36,10 @@ function Repo({ repo }: { repo: RepoType[number] }) {
   );
 
   return (
-    <li className="w-full px-0 sm:px-2" aria-label="repostory">
+    <li
+      className="mb-4 h-full w-full shrink-0 px-0 sm:w-1/2 sm:px-2"
+      aria-label="repostory"
+      onClick={() => console.log(repo)}>
       <div className="box flex-1 overflow-hidden p-3">
         <div className="flex h-full w-full flex-col">
           <div className="flex items-center [&>*]:fill-[#8b949e]">
@@ -42,30 +47,30 @@ function Repo({ repo }: { repo: RepoType[number] }) {
               <RepoIcon />
               <a
                 href={repo.html_url}
-                className="text-link mr-1 break-words font-[GeistMedium] text-[14px] hover:underline">
+                className="mr-1 break-words text-sm font-medium text-link hover:underline">
                 {repo.name}
               </a>
-              <span className="inline rounded-[2em] border border-[#30363d] p-[.11em_.5em] text-[11px] leading-4">
-                {repo.private ? "Private" : "Public"}
+              <span className="inline rounded-[2em] border border-[#30363d] p-[.11em_.5em] text-[11px] font-semibold capitalize leading-4">
+                {repo.visibility}
               </span>
             </div>
           </div>
-          <p className="mb-auto mt-2 inline-flex text-[13px]">
-            {repo.description}
-          </p>
+          <div className="mt-2 flex-1">
+            <p className="inline-flex text-[13px]">{repo.description}</p>
+          </div>
           <p className="mt-2 inline-flex items-center text-xs leading-4">
-            <span className="mr-3 inline-flex gap-1">
+            <span className="mr-3 inline-block">
               <span
                 className="repo-language-color"
                 style={{ backgroundColor: `${color?.color}` }}></span>
-              <span>{repo.language}</span>
+              <span> {repo.language}</span>
             </span>
             {repo.stargazers_count! >= 1 && (
               <a
                 href={`${repo.html_url}/stargazers`}
                 target="_blank"
-                className="flex items-center justify-end gap-1 [&>svg]:fill-[#8b949e]">
-                <Star />
+                className="flex items-center justify-end gap-1">
+                <Star className="fill-[#8b949e]" />
                 {repo.stargazers_count}
               </a>
             )}
@@ -73,8 +78,8 @@ function Repo({ repo }: { repo: RepoType[number] }) {
               <a
                 href={`${repo.html_url}/forks`}
                 target="_blank"
-                className="ml-4 flex items-center justify-center gap-1 [&>svg]:fill-[#8b949e]">
-                <Fork />
+                className="ml-4 flex items-center justify-center gap-1">
+                <Fork className="fill-[#8b949e]" />
                 {repo.forks_count}
               </a>
             )}
